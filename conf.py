c0fbb8dc0cce47168d27ad04842a49dda7b06233
JustaGist <saifksidhik@gmail.com>
2021-01-28 14:08:41 +0000

    update

--- conf.py
+++ conf.py
@@ -20,13 +20,13 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = u'Panda Robot'
-copyright = u'2020, Saif Sidhik'
+copyright = u'2019-2021, Saif Sidhik'
 author = u'Saif Sidhik'
 
 # The short X.Y version
-version = u'0.0.1'
+version = u'0.7.1'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1-beta'
+release = u'0.7.1-beta'
 
 
 # -- General configuration ---------------------------------------------------
